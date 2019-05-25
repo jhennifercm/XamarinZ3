8ededd416a0dbf7f4c9fe9a74954e5bb89d99c44
Jacqueline montano <Jacqueline montano@DESKTOP-9LLNFII>
2019-05-25 01:27:37 -0400

    al fin

--- XamarinZ3/app/app/App.xaml.cs
+++ XamarinZ3/app/app/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using app.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace app
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
